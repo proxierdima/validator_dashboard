fc729745f65dd1e2710833aacd0bc23ddf227c71
proxierdima <dokostiuchenko@gmail.com>
2026-03-10 05:07:11 +0200

    validator dashboard v0.6

---
+++ git.sh
@@ -0,0 +1,13 @@
+cd /mnt/c/Users/kostd/validator_dashboard
+
+git status
+git add .
+git commit -m "validator dashboard v0.6"
+git tag v0.6
+
+git remote remove origin 2>/dev/null
+git remote add origin git@github.com:proxierdima/validator_dashboard.git
+
+git branch -M main
+git push -u origin main
+git push origin v0.6
