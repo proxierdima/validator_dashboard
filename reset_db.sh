2840ba5da09540b4f25386023a7cd2418ca6e70a
proxierdima <dokostiuchenko@gmail.com>
2026-03-10 04:15:02 +0200

    modified:   reset_db.sh

--- reset_db.sh
+++ reset_db.sh
@@ -5,3 +5,7 @@ python3 -m scripts.load_chain_registry
 python3 -m scripts.load_tracked_networks
 python3 -m scripts.load_posthuman_endpoints
 python3 -m scripts.load_public_rpcs
+python3 -m app.collectors.endpoint_health_collector
+python3 -m app.collectors.validator_status_collector
+python3 -m app.services.network_status_aggregator
+python3 -m app.tasks.run_health_cycle
